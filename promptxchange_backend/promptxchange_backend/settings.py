8742cfe63f6928edfa2fcf8c29132b1d800756dd
Evans Ehiorobo <ehioroboevans@gmail.com>
2024-03-15 14:47:21 +0100

    secure application key.

--- promptxchange_backend/promptxchange_backend/settings.py
+++ promptxchange_backend/promptxchange_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from .config import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5x3@+3hqd+*_sm(c3a)b4ib!@&cp(8qs!4d@n9ibioi1()jrh7'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING TODO: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
