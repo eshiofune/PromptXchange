508edef5f73bf8fdb3a7c9e9845219536099c8ce
Jesuloluwa Solanke <jesuloluwap@gmail.com>
2024-03-19 22:17:37 +0100

    initial frontend structure

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto py-20">
+      <h1 className="text-4xl font-bold text-center text-gray-800">
+        Welcome to our PromptXchange App!
+      </h1>
+    </div>
+  );
+}
